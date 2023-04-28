413166beb013bb118e4ef0b3fe991908f6aff056
Patrick <patrickfbahia@hotmail.com>
2023-04-28 14:00:37 -0300

    editando postagens

--- routes/admin.js
+++ routes/admin.js
@@ -187,4 +187,62 @@ router.post("/postagens/nova", (req, res) => {
   }
 });
 
+router.get("/postagens/edit/:id", (req, res) => {
+  //Pesquisa no mongo pela postagem pegando o parametro id
+  Postagem.findOne({ _id: req.params.id })
+    .lean()
+    .then((postagem) => {
+      //Pesquisa no mongo pela categoria
+      Categoria.find()
+        .lean()
+        .then((categorias) => {
+          res.render("admin/editpostagens", {
+            categorias: categorias,
+            postagem: postagem,
+          });
+        })
+        .catch((err) => {
+          req.flash(
+            "error_msg",
+            "Houve um erro ao listar as categorias de edição"
+          );
+          res.redirect("/admin/postagens");
+        });
+    })
+    .catch((err) => {
+      req.flash(
+        "error_msg",
+        "Houve um erro ao carregar o formulário de edição"
+      );
+      res.redirect("/admin/postagens");
+    });
+});
+
+router.post("/postagem/edit", (req, res) => {
+  Postagem.findOne({ _id: req.body.id })
+    .then((postagem) => {
+      postagem.titulo = req.body.titulo;
+      postagem.slug = req.body.slug;
+      postagem.descricao = req.body.descricao;
+      postagem.conteudo = req.body.conteudo;
+      postagem.categoria = req.body.categoria;
+
+      postagem
+        .save()
+        .then(() => {
+          req.flash("success_msg", "Postagem editada com sucesso!");
+          res.redirect("/admin/postagens");
+        })
+        .catch((err) => {
+          req.flash("error_msg", "Erro interno");
+          res.redirect("/admin/postagens");
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      req.flash("error_msg", "Houve um erro ao salvar a edição");
+      res.redirect("/admin/postagens");
+    });
+});
+
 module.exports = router;
