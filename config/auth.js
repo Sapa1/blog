c35b31292763ac3895dc34745465060b55d9132f
Patrick <patrickfbahia@hotmail.com>
2023-05-29 13:09:25 -0300

    configurando passport

---
+++ config/auth.js
@@ -0,0 +1,37 @@
+const localStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+//Model de usuario
+require("../models/Usuario");
+const Usuario = mongoose.model("usuarios");
+
+module.exports = function (passport) {
+  passport.use(
+    new localStrategy({ usernameField: "email" }, (email, password, done) => {
+      Usuario.findOne({ email: email }).then((usuario) => {
+        if (!usuario) {
+          return done(null, false, { message: "Esta conta não existe" });
+        }
+
+        bcrypt.compare(senha, usuario.senha, (erro, batem) => {
+          if (batem) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: "Senha incorreta" });
+          }
+        });
+      });
+    })
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, usuario) => {
+      done(err, usuario);
+    });
+  });
+};
